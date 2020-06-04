b669983e97a1f431531b9ebc7c2dcdb9a1064829
Stefan Koev <stefan.m.koev@gmail.com>
2020-06-04 23:25:09 +0300

    Add files via upload

---
+++ Subqueries and JOINs/Exercises  Joins Subqueries.sql
@@ -0,0 +1,16 @@
+-- 1.Employee Address
+SELECT TOP(5) EmployeeID, JobTitle, e.AddressID, AddressText  FROM Employees AS e
+JOIN Addresses AS a ON a.AddressID  = e.AddressID
+ORDER BY AddressID ASC
+
+-- 2.Addresses with Towns
+SELECT TOP(50) FirstName, LastName, t.Name, AddressText FROM Employees AS e
+JOIN Addresses AS a ON a.AddressID  = e.AddressID
+JOIN Towns AS t ON t.TownID = a.TownID
+ORDER BY FirstName ASC, LastName
+
+-- 3.Sales Employee
+SELECT EmployeeID, FirstName, LastName, d.Name FROM Employees AS e
+JOIN Departments AS d ON d.DepartmentID = E.DepartmentID
+WHERE d.Name = 'Sales'
+ORDER BY EmployeeID ASC
