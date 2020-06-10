8c6df1d59e410d4a77f8aa14a36170b63917bb1a
Stefan Koev <stefan.m.koev@gmail.com>
2020-06-10 08:47:28 +0300

    Update Exercises Data Aggregation.sql

--- Indices and Data Aggregation/Exercises Data Aggregation.sql
+++ Indices and Data Aggregation/Exercises Data Aggregation.sql
@@ -102,3 +102,18 @@ FROM Employees
 WHERE HireDate > '2000-01-01'
 GROUP BY DepartmentID
 HAVING DepartmentID IN(2, 5, 7)
+
+-- 13. Employees Average Salaries
+SELECT * INTO EmployeesWithHighSalary FROM Employees
+WHERE Salary > 30000
+
+DELETE EmployeesWithHighSalary
+WHERE ManagerID = 42
+
+UPDATE EmployeesWithHighSalary
+SET Salary += 5000
+WHERE DepartmentID = 1
+
+SELECT DepartmentID, AVG(Salary) AS [AverageSalary]
+FROM EmployeesWithHighSalary
+GROUP BY DepartmentID
