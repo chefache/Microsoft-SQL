440d3f2deca5b400d2487b2df2ee1377ed8b1a7e
Stefan Koev <stefan.m.koev@gmail.com>
2020-06-12 18:41:04 +0300

    Update Exercises  Functions and Stored Procedures.sql

--- Functions and Stored Procedures/Exercises  Functions and Stored Procedures.sql
+++ Functions and Stored Procedures/Exercises  Functions and Stored Procedures.sql
@@ -7,4 +7,13 @@ FROM Employees AS e
 WHERE e.Salary > 35000
 END
 
+-- 2.Employees with Salary Above Number
+CREATE PROC usp_GetEmployeesSalaryAboveNumber(@chaeckAmount DECIMAL(18,4))
+AS
+BEGIN
+SELECT FirstName, LastName
+FROM Employees AS e
+WHERE e.Salary >= @chaeckAmount
+END
 
+EXEC usp_GetEmployeesSalaryAboveNumber 48100
