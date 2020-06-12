a5fee5543ac64c3f40bda0193a47274571ebd6e0
Stefan Koev <stefan.m.koev@gmail.com>
2020-06-12 18:42:39 +0300

    Update Exercises  Functions and Stored Procedures.sql

--- Functions and Stored Procedures/Exercises  Functions and Stored Procedures.sql
+++ Functions and Stored Procedures/Exercises  Functions and Stored Procedures.sql
@@ -42,3 +42,29 @@ WHERE t.Name = @townName
 END
 
 EXEC usp_GetEmployeesFromTown 'Sofia'
+
+-- 5. Salary Level Function
+CREATE FUNCTION ufn_GetSalaryLevel(@salary DECIMAL(18, 4))
+RETURNS VARCHAR(7)
+AS
+BEGIN
+	DECLARE @salaryLevel VARCHAR(7);
+	IF(@salary < 30000)
+	 BEGIN
+	  SET @salaryLevel = 'Low';
+ 	 END
+	ELSE IF(@salary >= 30000 AND @salary <= 50000)
+	 BEGIN
+	  SET @salaryLevel = 'Average';
+ 	 END
+	ELSE
+	 BEGIN
+	  SET @salaryLevel = 'High'
+	 END
+  RETURN @salaryLevel;
+END
+
+SELECT FirstName,
+	   LastName,
+	   dbo.ufn_GetSalaryLevel(Salary) AS [SalaryLevel]
+FROM Employees
