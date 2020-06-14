7b464d8f2296067dd0ce857d3551223d1b117b8a
Stefan Koev <stefan.m.koev@gmail.com>
2020-06-14 21:54:37 +0300

    Update Exercises  Functions and Stored Procedures.sql

--- Functions and Stored Procedures/Exercises  Functions and Stored Procedures.sql
+++ Functions and Stored Procedures/Exercises  Functions and Stored Procedures.sql
@@ -103,4 +103,47 @@ BEGIN
 	 RETURN 1;
 END
 
-SELECT dbo.ufn_IsWordComprised('oistmiahf','Sofia')				    
+SELECT dbo.ufn_IsWordComprised('oistmiahf','Sofia')
+			
+-- 8.* Delete Employees and Departments
+CREATE PROC usp_DeleteEmployeesFromDepartment (@departmentId INT)
+AS
+BEGIN
+
+	DELETE FROM EmployeesProjects
+	WHERE EmployeeID IN (
+	                      SELECT EmployeeID FROM Employees
+                          WHERE DepartmentID = @departmentId
+	                    )
+	UPDATE Employees
+	SET ManagerID = NULL
+	WHERE ManagerID IN (
+	                     SELECT EmployeeID FROM Employees
+                         WHERE DepartmentID = @departmentId
+	                   )
+
+	ALTER TABLE Departments
+	ALTER COLUMN ManagerID INT
+
+
+	UPDATE Departments
+	SET ManagerID = NULL
+	WHERE ManagerID IN (
+	                     SELECT EmployeeID FROM Employees
+                         WHERE DepartmentID = @departmentId
+	                   )
+
+DELETE FROM Employees
+WHERE DepartmentID = @departmentId
+
+DELETE FROM Departments
+WHERE DepartmentID = @departmentId
+
+SELECT COUNT(*) FROM Employees
+WHERE DepartmentID = @departmentId
+
+END
+
+EXEC usp_DeleteEmployeesFromDepartment 1
+
+			
