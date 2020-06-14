5d37ef85b195434815db71a4a643e85033655619
Stefan Koev <stefan.m.koev@gmail.com>
2020-06-14 21:56:39 +0300

    Update Exercises  Functions and Stored Procedures.sql

--- Functions and Stored Procedures/Exercises  Functions and Stored Procedures.sql
+++ Functions and Stored Procedures/Exercises  Functions and Stored Procedures.sql
@@ -198,3 +198,23 @@ BEGIN
 END
 
 EXEC dbo.usp_CalculateFutureValueForAccount 1, 0.1			
+
+-- 13. *Scalar Function: Cash in User Games Odd Rows
+CREATE OR ALTER FUNCTION ufn_CashInUsersGames( @gameName NVARCHAR(50))
+RETURNS TABLE AS
+RETURN SELECT
+	        (
+             SELECT SUM(Cash) AS [SumCash] FROM 
+			   (
+			     SELECT g.[Name], 
+                 ug.Cash, 
+                 ROW_NUMBER() OVER (PARTITION BY g.Name ORDER BY ug.Cash DESC) AS [RowNum]
+                 FROM UsersGames AS ug
+                 JOIN Games AS g ON g.Id = ug.GameId
+                 WHERE g.[Name] = @gameName
+               ) AS [RowNumQuery] 
+  WHERE RowNum % 2 <> 0
+) AS [SumCash]
+
+SELECT * FROM dbo.ufn_CashInUsersGames('Love in a mist')
+			
